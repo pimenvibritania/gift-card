0470de9cc2263b3fcbd339c7ccd69181a340a8f9
pimenvibritania <pimenvibritania@gmail.com>
2022-02-19 16:30:06 +0700

    edit pipelines deployer

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-
 app_dir="/home/pimenvibritania/gift-card"
 
 #check if folder exist
 if [ ! -d "$app_dir" ]; then
     cd /home/pimenvibritania && git clone git@bitbucket.org:pimenvibritania13/gift-card.git
+    init_install
 else
     cd $app_dir && git pull origin development
+    composer install -vvv
 fi
 
 init_install() {
     composer install -vvv
+    openssl enc -aes-256-cbc -d -in .secret.enc -out .env -k ${AES_PASS}
     php artisan migrate:fresh --seed
     php artisan jwt:secret
 }
